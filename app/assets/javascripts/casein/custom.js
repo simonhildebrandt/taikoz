958d4ad036835c5c2fa41c2bab5cf6c3cc3d08c5
Rob Cornish <rob@robcornishmusic.com>
2015-09-30 08:33:04 +1000

    wysihtml5 working, html_safe display

--- app/assets/javascripts/casein/custom.js
+++ app/assets/javascripts/casein/custom.js
@@ -1,2 +1,17 @@
 // CASEIN CUSTOM
 // Use this file for your project-specific Casein JavaScript
+
+//= require bootstrap-wysihtml5
+
+$('.wysihtml5').wysihtml5()
+
+// $(document).ready(function(){
+//
+//   $('.wysihtml5').each(function(i, elem) {
+//     $(elem).wysihtml5();
+//   });
+//
+// })
+// $(document).on('page:load', function(){
+// window['rangy'].initialized = false
+// })
