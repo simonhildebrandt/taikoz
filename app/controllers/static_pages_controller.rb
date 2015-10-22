6a6bcc88d30152d2208829ccac1728d440625ce8
Rob Cornish <rob@robcornishmusic.com>
2015-10-23 00:17:02 +1100

    news index layout, sticky sidebar boundry

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,5 @@
 class StaticPagesController < ApplicationController
+  before_action :sidebar
 
   def privacy_policy
   end
@@ -7,6 +8,12 @@ class StaticPagesController < ApplicationController
   end
 
   def donors
+  end
+
+private
+
+  def sidebar
+    @events = Event.order(start_date: :asc).current_events.published.limit(2)
   end
 
 end
