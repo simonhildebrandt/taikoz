fa882571be6526231408a4e792ee1ee797e68969
Rob Cornish <rob@robcornishmusic.com>
2015-10-25 16:40:14 +1100

    ENV1

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -78,11 +78,11 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "localhost:3000" }
+  config.action_mailer.default_url_options = { host: "http://taikoz.herokuapp.com" }
   config.action_mailer.smtp_settings = {
   address:              'smtp.api.createsend.com',
   port:                 587,
-  domain:               'taikoz.com',
+  domain:               'taikoz.herokuapp.com',
   user_name:            ENV["CAMPAIGN_MONITOR"],
   password:             ENV["CAMPAIGN_MONITOR"],
   authentication:       'plain',
